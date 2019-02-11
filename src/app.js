01a64a8e56b9046f02f8d05c9b007244b095f440
Thomas Lawless <tlawless94@gmail.com>
2019-02-11 14:12:50 -0600

    initialize

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import '@babel/polyfill'
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+import cors from 'cors'
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors({
+  origin: process.env.ORIGIN_URL,
+  optionsSuccessStatus: 200
+}))
+
+app.use('/', (req, res, next) => {
+  res.send('working')
+})
+
+app.use('/api/v1.0.0', require('./api'))
+
+app.use(express.static(path.join(__dirname, 'assets')))
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
