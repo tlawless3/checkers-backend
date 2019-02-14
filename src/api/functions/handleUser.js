63a0c3dd9c4cb55df242c6d2b238203ee8b54766
Thomas Lawless <tlawless94@gmail.com>
2019-02-14 15:08:05 -0600

    create login function

--- src/api/functions/handleUser.js
+++ src/api/functions/handleUser.js
@@ -2,16 +2,39 @@ import db from '../../db/index'
 
 const login = async (req, res) => {
   if (req.body.user) {
-    const foundUser = await db.sequelize.user.findOne({
-      where: {
-        username: req.body.username
+    try {
+      const foundUser = await db.sequelize.user.findOne({
+        where: {
+          username: req.body.username
+        }
+      })
+      if (foundUser.correctPassword()) {
+        const returnUser = {
+          username: foundUser.username,
+          role: foundUser.role,
+          displayName: foundUser.displayName,
+          userId: foundUser.id
+        }
+        const token = jwt.sign(returnUser, process.env.SECRET)
+        res.cookie('userToken', token)
+        res.status('200')
+        res.send('login successful')
+      } else {
+        res.status('401')
+        res.send('incorrect password')
       }
-    })
-    if (foundUser.correctPassword()) {
-      jwt
+    } catch (err) {
+      res.status('404')
+      res.send(err)
     }
   } else {
     res.status('500')
     res.send('malformed login data')
   }
+}
+
+//create user function
+
+module.exports = {
+  login
 }
--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import {
+  login
+} from '../functions/handleUser'
 
 const app = express()
 
 app.get('/login', (req, res, next) => {
-
+  login(req, res)
+  next()
 })
 
 app.post('/create', (req, res, next) => {
